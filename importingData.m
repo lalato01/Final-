16fde75b52e117f09ddbbd9b2f63a2a90517fb78
lalato01 <45377294+lalato01@users.noreply.github.com>
2019-01-11 19:31:52 -0500

    Update importingData.m

--- importingData.m
+++ importingData.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function orgCpp = importingData(rawCppData)
 %% Help Documentation importingData
 % The function importingData obtains the raw cpp data and creates a struct that will
@@ -19,11 +20,26 @@ function orgCpp = importingData(rawCppData)
 orgCpp.xlsName = rawCppData;
 % Select range of data
 if strcmpi('_CPP Piloting Trial 1.xlsx',orgCpp.xlsName) == 1 % Case insensitive comparison
+=======
+function [orgCpp,cppData,secShifts,subjs,numSubjs] = importingData(rawCppData)
+%% Help Documentation importingData
+% The function importingData obtains the raw cpp data and creates a struct that will
+% contain matrices further data storage and processing. It takes for an input the filename
+% and returns the struct orgCpp, the raw data format in a cell array table, the number
+% of sector shifts, and subjects.
+
+%% Importing data & creating struct
+% Name of xls sheet currently being analyzed
+orgCpp.xlsName = rawCppData;
+% Select range of data
+if orgCpp.xlsName == '_CPP Piloting Trial 1.xlsx'
+>>>>>>> 66adf2bad35ee6f8612dd2f32a2bd85ca8a620b8
     rawData = 'E1:AJ37';
 else
     rawData = 'E1:R28';
 end
 % Obtain all raw data from file
+<<<<<<< HEAD
 [~, ~, orgCpp.cppRawData] = xlsread(rawCppData,'CPP Format',rawData);
 
 orgCpp.cppRawData(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),orgCpp.cppRawData)) = {''};
@@ -37,10 +53,26 @@ orgCpp.cppRawData(1,:)=[]; % Delete first row after animal ID's have been stored
 [secShifts,subjs] = size(orgCpp.cppRawData);
 orgCpp.numSubjs = subjs/2;
 orgCpp.sectorMtxs = NaN(secShifts,orgCpp.numSubjs,2);
+=======
+[~, ~, cppData] = xlsread(rawCppData,'CPP Format',rawData);
+
+cppData(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),cppData)) = {''};
+
+% Create ouput argument as a struct, with first field containing subject ID's
+orgCpp.animalID = cppData(1,:)';
+orgCpp.animalID(cellfun('isempty', orgCpp.animalID)) = [];
+
+% Make matrices for each compartment with the corresponding dimensions
+cppData(1,:)=[]; % Delete first row after animal ID's have been stored
+[secShifts,subjs] = size(cppData);
+numSubjs = subjs/2;
+orgCpp.sectorMtxs = NaN(secShifts,numSubjs,2);
+>>>>>>> 66adf2bad35ee6f8612dd2f32a2bd85ca8a620b8
 
 % Each column will correspond to one subject and the rows in that column to the amount of compartment shifts.
 % Each cell contains the duration, in seconds, that the animal spent in that compartment before switching.
 
+<<<<<<< HEAD
 orgCpp = storingDurs(orgCpp,orgCpp.cppRawData,secShifts,subjs); % Store duration data
 
 function orgCpp = storingDurs(orgCpp,cppData,secShifts,subjs)
@@ -110,3 +142,5 @@ end
 
 % State machine status
 
+=======
+>>>>>>> 66adf2bad35ee6f8612dd2f32a2bd85ca8a620b8
