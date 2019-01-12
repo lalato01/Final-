03dbfd8fd7f3e76a2509ce125bef7929b28e6701
lalato01 <45377294+lalato01@users.noreply.github.com>
2019-01-11 19:31:58 -0500

    Update dataAnalysis.m

--- dataAnalysis.m
+++ dataAnalysis.m
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function analyzedCpp = dataAnalysis(orgCpp)
 %% Help Documentation dataAnalysis
 % The function dataAnalysis computes the results for cpp data experiments.
@@ -59,4 +60,31 @@ if strcmpi(graphOption, 'Yes')
 else
     return;
 end
+=======
+function [orgCpp,indvSubjDur] = dataAnalysis(orgCpp,numSubjs)
+%% Help Documentation dataAnalysis
+% The function dataAnalysis computes the results for cpp data experiments.
+% It takes for an input the output variables from the importingData and storingDurs functions 
+% and returns as an output the finalized struct with all the information regarding the 
+% computations done on the data. It also outputs the final cohort compartment preferences. 
+
+%% Data Analysis
+% This will be made into an image matching JNeurosci formatting requirements
+% Sum the durations of each animal
+orgCpp.MtxsColSums = nansum(orgCpp.sectorMtxs);
+% Total duration per animal
+indvSubjDur = 1:1:numSubjs;
+orgCpp.indvTotalDur = orgCpp.MtxsColSums(1,indvSubjDur,1) + orgCpp.MtxsColSums(1,indvSubjDur,2); % sum of corresponding pinkMtxColSums + blueMtxColSums
+% Indv animal compartment percentage
+orgCpp.indvPref = ((orgCpp.MtxsColSums./orgCpp.indvTotalDur).*100);
+% Get cohort pink vs. blue preference
+orgCpp.grpSectorPref =((sum(orgCpp.MtxsColSums))./(sum(orgCpp.indvTotalDur))).*100;
+
+% Sanity check: all fields created above need to equal the amount of subjects
+if length(orgCpp.animalID) ~= numSubjs && length(orgCpp.MtxsColSums) ~= numSubjs && length(orgCpp.indvTotalDur) ~= numSubjs && length(orgCpp.indvPref) ~= numSubjs
+    warning('Vector elements vary in length: durations not properly obtained')
+end
+orgCpp % Final struct and fields 
+fprintf('Pink cohort preference: %.2f%% \nBlue cohort preference: %.2f%%\n\n', orgCpp.grpSectorPref(:,:,1), orgCpp.grpSectorPref(:,:,2))
+>>>>>>> 66adf2bad35ee6f8612dd2f32a2bd85ca8a620b8
 
